9df57bfefbe2fd8371bbd57ea6b9760acdd334f3
Stacy Carr <stacy@boostersinabox.com>
2018-07-09 17:57:40 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,5 @@
-numbers = [1,2,3]
 def square_array(array)
-  array.each do |numbers| 
-    result = numbers**2
-    new_array = ["#{result}","#{result}","#{result}"]
-  end
+  squared = []
+  array.each { |element| squared << element ** 2 }
+  squared
 end
